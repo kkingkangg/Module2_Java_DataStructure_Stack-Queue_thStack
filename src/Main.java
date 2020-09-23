697cb6f355105d0736a4f5e6cd9f918f6af2724a
kkingkangg <phungnghia152@gmail.com>
2020-09-23 22:29:13 +0700

    thực hành triển khai Stack sử dụng class LinkedList

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("1. Stack of integers");
+        MyGenericStack.stackOfIntegers();
+        System.out.println("\n2. Stack of Strings");
+        MyGenericStack.stackOfIStrings();
+    }
+}
+
